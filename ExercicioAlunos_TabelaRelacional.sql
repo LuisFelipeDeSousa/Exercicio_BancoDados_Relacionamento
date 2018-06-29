3fcbfa9fed53717d5c13b1d454759d3883601f62
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-29 17:26:54 -0300

    fdfdsf

---
+++ ExercicioAlunos_TabelaRelacional.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE alunos(
+
+id                 INT IDENTITY, 
+nome			   VARCHAR(200),	
+idade              INT 
+);
+
+CREATE TABLE caracteristica(
+
+id					INT IDENTITY,
+id_alunos			INT,
+nome				VARCHAR(150)
+);
+
